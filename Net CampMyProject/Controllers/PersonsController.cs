1ef6a942a3a7490e2a6f26da8dfc7252bcabef2f
mykhniuk <mikhnjuk1994@gmail.com>
2021-12-15 01:39:22 +0200

    - Person details page access fix

--- Net CampMyProject/Controllers/PersonsController.cs
+++ Net CampMyProject/Controllers/PersonsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Net_CampMyProject.Data.Models;
 using Net_CampMyProject.Models;
 using Net_CampMyProject.Services.Interfaces;
@@ -10,6 +12,12 @@ namespace Net_CampMyProject.Controllers
     {
         public PersonsController(IPersonsRepository personRepository) : base(personRepository)
         {
+        }
+
+        [AllowAnonymous]
+        public override Task<IActionResult> Details(int id)
+        {
+            return base.Details(id);
         }
     }
 }
